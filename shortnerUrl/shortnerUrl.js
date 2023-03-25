81336abe2d11d16764e80681972eb9b281aa1688
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-25 22:38:15 +0200

    shortner url functions

--- shortnerUrl/shortnerUrl.js
+++ shortnerUrl/shortnerUrl.js
@@ -1,3 +1,12 @@
 const originalUrls = [];
 const shortUrls = [];
 
+
+function validateUrl(value) {
+	var urlPattern = new RegExp(
+		"^(https?:\\/\\/)?" + "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|)"
+	);
+
+	return !!urlPattern.test(value);
+}
+
