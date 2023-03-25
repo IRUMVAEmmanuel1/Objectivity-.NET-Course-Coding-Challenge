319d5f937b98ee7a0323581130fbbf66e9b35f32
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-25 22:38:48 +0200

    shortner url functions

--- shortnerUrl/shortnerUrl.js
+++ shortnerUrl/shortnerUrl.js
@@ -9,4 +9,55 @@ function validateUrl(value) {
 
 	return !!urlPattern.test(value);
 }
+function getrandom() {
+	var random_string =
+		Math.random().toString(32).substring(2, 5) +
+		Math.random().toString(32).substring(2, 5);
+	return "https://" + random_string + ".com";
+}
 
+const handleUrlShorten = (url) => {
+	let finalUrl = {};
+
+	if (validateUrl(url)) {
+		let randomString = getrandom();
+
+		finalUrl = {
+			shorten: randomString,
+			original: url,
+		};
+	}
+
+	return finalUrl;
+};
+
+const { shorten } = handleUrlShorten();
+
+function shortenUrl(longUrl) {
+	const index = originalUrls.indexOf(longUrl);
+	if (index > -1) {
+		return shortUrls[index];
+	} else {
+		const shortUrl = getrandom();
+		originalUrls.push(longUrl);
+		shortUrls.push(shortUrl);
+		return shortUrl;
+	}
+}
+
+function redirect(shortUrl) {
+	const index = shortUrls.indexOf(shortUrl);
+	if (index > -1) {
+		window.location.href = originalUrls[index];
+	} else {
+		alert(
+			"Your URL does not appear to be a valid format. Please try again with a valid URL"
+		);
+	}
+}
+const longUrl = "https://app.slack.com/client/T03DU70TCTY/C03EDF7QEJV";
+
+const shortUrl = shortenUrl(longUrl);
+console.log(shortUrl);
+
+redirect(shortUrl);
